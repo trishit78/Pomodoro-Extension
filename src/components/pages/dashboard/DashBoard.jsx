673c3b2b451ec4e823c0de6a9cc1c54cb6148719
anurag47 <anurag.patil@coradius.io>
2023-10-12 16:06:45 +0530

    added script from linux/mac terminal in package.json & integreted chrome local storage

--- src/components/pages/dashboard/DashBoard.jsx
+++ src/components/pages/dashboard/DashBoard.jsx
@@ -1,10 +1,24 @@
+import { useLocation } from "react-router-dom";
 import Timer from "./Timer";
+import { useEffect, useState } from "react";
 
 
-function DashBoard() {
+function DashBoard(props) {
+  const location = useLocation()
+  const { state } = location
+  const [data, setData] = useState()
+  useEffect(() => {
+    chrome.storage.local.get([`${state}`]).then((res) => setData(res[state]))
+  },[])
+  console.log(val, br)
+  const val = parseInt(data.durationValue)
+  const br = parseInt(data.breakTime)
   return (
     <>
-      <Timer />
+      <Timer
+        taskDuration={val}
+        breakTime={br}
+      />
     </>
   );
 }
